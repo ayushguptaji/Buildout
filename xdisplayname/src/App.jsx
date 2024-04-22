9f80c41dd98e10144412082266a2695778575790
Ayush Gupta <gupta.ayush0111@gmail.com>
2024-04-22 14:03:15 +0530

    Created xdisplayname

---
+++ xdisplayname/src/App.jsx
@@ -0,0 +1,34 @@
+import './App.css';
+import { useState } from "react";
+
+const App = () => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [fullName, setFullName] = useState("");
+  const [classValue, setClassValue] = useState("hide");
+
+  const createFullName = (e) => {
+    e.preventDefault();
+    setClassValue("show");
+    setFullName(firstName+" "+lastName);
+  }
+  return (
+    <>
+      <h1>Full Name Display</h1>
+      <form onSubmit={createFullName}>
+        <label for="firstName">First Name:</label>
+        <input type="text" name="firstName" id="firstName" value={firstName} onChange={(e) => setFirstName(e.target.value)} required/>
+        <br />
+        <label for="lastName">Last Name:</label>
+        <input type="text" name="lastName" id="lastName" value={lastName} onChange={(e) => setLastName(e.target.value)} required/>
+        <br />
+        <button type="submit">Submit</button>
+      </form>
+      <div className={classValue}>
+        Full Name: {fullName}
+      </div>
+    </>
+  );
+}
+
+export default App;
