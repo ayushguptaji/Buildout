e7c0f0726c85287850b027a3d157c6f0bc1f0cdc
Ayush Gupta <gupta.ayush0111@gmail.com>
2024-04-22 14:33:06 +0530

    Solved EdgeTest1

--- xdisplayname/src/App.jsx
+++ xdisplayname/src/App.jsx
@@ -9,12 +9,8 @@ const App = () => {
 
   const createFullName = (e) => {
     e.preventDefault();
-    if (firstName === "" || lastName === "") {
-      setClassValue("hide");
-      return;
-    }
     setClassValue("show");
-    setFullName(firstName + " " + lastName);
+    setFullName("Full Name: " + firstName + " " + lastName);
   };
   return (
     <>
@@ -27,7 +23,10 @@ const App = () => {
           id="firstName"
           value={firstName}
           onChange={(e) => {
-            if (e.target.value === "") setClassValue("hide");
+            if (e.target.value === "") {
+              setClassValue("hide");
+              setFullName("");
+            }
             setFirstName(e.target.value);
           }}
           required
@@ -40,7 +39,10 @@ const App = () => {
           id="lastName"
           value={lastName}
           onChange={(e) => {
-            if (e.target.value === "") setClassValue("hide");
+            if (e.target.value === "") {
+              setClassValue("hide");
+              setFullName("");
+            }
             setLastName(e.target.value);
           }}
           required
@@ -48,7 +50,7 @@ const App = () => {
         <br />
         <button type="submit">Submit</button>
       </form>
-      <div className={classValue}>Full Name: {fullName}</div>
+      <div className={classValue}>{fullName}</div>
     </>
   );
 };
