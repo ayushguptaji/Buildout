6b89c47bdf42e52b6368b201fbab59f235832d60
Ayush Gupta <gupta.ayush0111@gmail.com>
2024-04-22 14:18:44 +0530

    Solved EdgeTest

--- xdisplayname/src/App.jsx
+++ xdisplayname/src/App.jsx
@@ -1,4 +1,4 @@
-import './App.css';
+import "./App.css";
 import { useState } from "react";
 
 const App = () => {
@@ -9,26 +9,48 @@ const App = () => {
 
   const createFullName = (e) => {
     e.preventDefault();
+    if (firstName === "" || lastName === "") {
+      setClassValue("hide");
+      return;
+    }
     setClassValue("show");
-    setFullName(firstName+" "+lastName);
-  }
+    setFullName(firstName + " " + lastName);
+  };
   return (
     <>
       <h1>Full Name Display</h1>
       <form onSubmit={createFullName}>
         <label for="firstName">First Name:</label>
-        <input type="text" name="firstName" id="firstName" value={firstName} onChange={(e) => setFirstName(e.target.value)} required/>
+        <input
+          type="text"
+          name="firstName"
+          id="firstName"
+          value={firstName}
+          onChange={(e) => {
+            if (e.target.value === "") setClassValue("hide");
+            setFirstName(e.target.value);
+          }}
+          required
+        />
         <br />
         <label for="lastName">Last Name:</label>
-        <input type="text" name="lastName" id="lastName" value={lastName} onChange={(e) => setLastName(e.target.value)} required/>
+        <input
+          type="text"
+          name="lastName"
+          id="lastName"
+          value={lastName}
+          onChange={(e) => {
+            if (e.target.value === "") setClassValue("hide");
+            setLastName(e.target.value);
+          }}
+          required
+        />
         <br />
         <button type="submit">Submit</button>
       </form>
-      <div className={classValue}>
-        Full Name: {fullName}
-      </div>
+      <div className={classValue}>Full Name: {fullName}</div>
     </>
   );
-}
+};
 
 export default App;
