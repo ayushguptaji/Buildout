bf3f677367267e2a9939b3ce01fd4e4a06023d2b
Ayush Gupta <gupta.ayush0111@gmail.com>
2024-04-22 22:55:56 +0530

    Created xstopwatch

--- xstopwatch/src/App.js
+++ xstopwatch/src/App.js
@@ -1,10 +1,56 @@
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [time, setTime] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+
+  const startStop = () => {
+    setIsRunning(!isRunning);
+  };
+
+  const reset = () => {
+    setIsRunning(false);
+    setTime(0);
+  };
+
+  const formatTime = (seconds) => {
+    let sec = seconds % 60;
+    const mins = Math.floor(seconds / 60);
+    if (sec < 10) {
+      sec = "0" + sec;
+    }
+    return `${mins}:${sec}`;
+  };
+
+  useEffect(()=> {
+    let intervalId;
+    if(isRunning) {
+      intervalId = setInterval(() => {
+        setTime((prev) => prev+1);
+      }, 1000);
+    }
+    return () => {
+      clearInterval(intervalId);
+    }
+  }, [isRunning])
   return (
-    <div>
-      
-    </div>
+    <>
+      <h1>Stopwatch</h1>
+      <p>Time: {formatTime(time)}</p>
+      {!isRunning ? (
+        <button name="Start" onClick={startStop}>
+          Start
+        </button>
+      ) : (
+        <button name="Stop" onClick={startStop}>
+          Stop
+        </button>
+      )}
+      <button name="Reset" onClick={reset}>
+        Reset
+      </button>
+    </>
   );
 }
 
