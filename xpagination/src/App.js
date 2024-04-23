42baaf17b0f97cc1df5d76c658dc7765899f4c3f
Ayush Gupta <gupta.ayush0111@gmail.com>
2024-04-23 18:50:42 +0530

    Completed xpagination

--- xpagination/src/App.js
+++ xpagination/src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import Table from "./components/Table";
 
 function App() {
+  const [data, setData] = useState([]);
+  const [loader, setLoader] = useState(true);
+
+  const fetchData = async () => {
+    setLoader(true);
+    await fetch("https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json")
+      .then((response) => response.json())
+      .then((newData) => {
+        setData(newData);
+      })
+      .catch((error) => {
+        console.error("Error fetching data: ", error);
+        alert("failed to fetch data");
+      });
+    setLoader(false);
+  };
+
+  useEffect(() => {
+    let isFlag = true;
+    if (isFlag) {
+      fetchData();
+    }
+    return () => {
+      isFlag = false;
+    };
+    // eslint-disable-next-line
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {loader === true ? (
+        <div className="loader">Data Loading........</div>
+      ) : (
+        <div id="main-page">
+          <h1>Employee Data Table</h1>
+          <Table totalData={data}/>
+        </div>
+      )}
+    </>
   );
 }
 
---
+++ xpagination/src/components/Table.jsx
@@ -0,0 +1,80 @@
+import { useEffect, useState } from "react";
+import "./Table.css";
+
+function Table({ totalData }) {
+  const [data, setData] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [disablePrevious, setDisablePrevious] = useState(true);
+  const [disableNext, setDisableNext] = useState(false);
+
+  useEffect(() => {
+    currentPage > 1 ? setDisablePrevious(false) : setDisablePrevious(true);
+    currentPage === Math.ceil(data.length/10) ? setDisableNext(true) : setDisableNext(false);
+  }, [currentPage, data]);
+
+  useEffect(() => {
+    let isFlag = true;
+    if (isFlag) {
+      setData(totalData);
+    }
+    return () => {
+      isFlag = false;
+    };
+    // eslint-disable-next-line
+  }, []);
+
+  return (
+    <>
+      <table className="table-container">
+        <tbody>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Role</th>
+          </tr>
+        </tbody>
+        <tbody>
+          {data
+            .filter(
+              (val) =>
+                val.id >= currentPage * 10 - 9 && val.id < currentPage * 10 + 1
+            )
+            .map((val) => {
+              return (
+                <tr key={val.id}>
+                  <td>{val.id}</td>
+                  <td>{val.name}</td>
+                  <td>{val.email}</td>
+                  <td>{val.role}</td>
+                </tr>
+              );
+            })}
+        </tbody>
+      </table>
+      <div>
+        <button
+          name="Previous"
+          disabled={disablePrevious}
+          onClick={() => {
+            setCurrentPage((prev) => prev - 1);
+          }}
+        >
+          Previous
+        </button>
+        <button name="Current">{currentPage}</button>
+        <button
+          name="Next"
+          disabled={disableNext}
+          onClick={() => {
+            setCurrentPage((prev) => prev + 1);
+          }}
+        >
+          Next
+        </button>
+      </div>
+    </>
+  );
+}
+
+export default Table;
