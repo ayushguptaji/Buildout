17ba91bbfc22ebd22d6e79b45ca71dcaa6f15281
Ayush Gupta <gupta.ayush0111@gmail.com>
2024-04-24 16:01:03 +0530

    Solved a bug in search field

--- xdictionary/src/App.js
+++ xdictionary/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import data from './data';
 
 function App() {
-  const [definition, setDefinition] = useState("Definition:");
+  const [definition, setDefinition] = useState("");
   const [search, setSearch] = useState("");
   const [showMessage, setShowMessage] = useState(false);
 
@@ -11,12 +11,12 @@ function App() {
     let found = false;
     data.forEach((val) => {
       if(val.word.toLowerCase() === search.toLowerCase()) {
-        setDefinition((prev) => prev+val.meaning);
+        setDefinition((prev) => val.meaning);
         found = true;
       }
     })
     if(!found) {
-      setDefinition("Definition:");
+      setDefinition("");
       setShowMessage(true); 
     } else {
       setShowMessage(false);
@@ -28,6 +28,7 @@ function App() {
       <h1>Dictionary App</h1>
       <input type="text" placeholder='Search for a word...' value={search} onChange={(e) => setSearch(e.target.value)}/>
       <button type='button' name='Search' onClick={searchWord}>Search</button>
+      <h5>Definition:</h5>
       <p>{definition}</p>
       {showMessage? <p>Word not found in the dictionary.</p>: null}
     </>
