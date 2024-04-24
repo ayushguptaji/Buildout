e90e5033c89359cf9e0f60faaeb6dd1e3cf40a28
Ayush Gupta <gupta.ayush0111@gmail.com>
2024-04-24 21:43:20 +0530

    Completed xstates

--- xstates/src/App.js
+++ xstates/src/App.js
@@ -1,24 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  const [selectedCountry, setSelectedCountry] = useState("none");
+  const [states, setStates] = useState([]);
+  const [selectedState, setSelectedState] = useState("none");
+  const [cities, setCities] = useState([]);
+  const [selectedCity, setSelectedCity] = useState("none");
+  
+  useEffect(() => {
+    let isFlag = true;
+    const fetchCity = async () => {
+      if(selectedState === "none") {
+        return;
+      }
+      await fetch(`https://crio-location-selector.onrender.com/country=${selectedCountry}/state=${selectedState}/cities`)
+      .then((response) => response.json())
+      .then((data) => setCities(data))
+      .catch((error) => console.error("Error fetching cities: ", error));
+    }
+    if (isFlag) {
+      fetchCity();
+    }
+    return () => {
+      isFlag = false;
+    };
+  }, [selectedState, selectedCountry]);  
+
+  useEffect(() => {
+    let isFlag = true;
+    const fetchState = async () => {
+      if(selectedCountry === "none") {
+        return;
+      }
+      await fetch(`https://crio-location-selector.onrender.com/country=${selectedCountry}/states`)
+      .then((response) => response.json())
+      .then((data) => setStates(data))
+      .catch((error) => console.error("Error fetching states: ", error));
+    }
+    if (isFlag) {
+      fetchState();
+    }
+    return () => {
+      isFlag = false;
+    };
+  }, [selectedCountry]);
+
+  useEffect(() => {
+    let isFlag = true;
+    const fetchCountry = async () => {
+      await fetch("https://crio-location-selector.onrender.com/countries")
+      .then((response) => response.json())
+      .then((data) => setCountries(data))
+      .catch((error) => console.error("Error fetching countries: ", error));
+    }
+    if (isFlag) {
+      fetchCountry();
+    }
+    return () => {
+      isFlag = false;
+    };
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="main">
+        <h1>Select Location</h1>
+        <div className="dropdowns">
+          <select name="country" id="country" value={selectedCountry} onChange={(e) => setSelectedCountry(e.target.value)} className="dropdown">
+            <option value="none" disabled>Select Country</option>
+            {countries.map((val, idx) => {
+              return <option value={val} key={idx}>{val}</option>;
+            })}
+          </select>
+          <select name="state" id="state" disabled={selectedCountry === "none"?true:false} value={selectedState} onChange={(e) => setSelectedState(e.target.value)} className="dropdown">
+            <option value="none" disabled>Select State</option>
+            {states.map((val, idx) => {
+              return <option value={val} key={idx}>{val}</option>;
+            })}
+          </select>
+          <select name="city" id="city" disabled={selectedState === "none"?true:false} value={selectedCity} onChange={(e) => setSelectedCity(e.target.value)} className="dropdown">
+            <option value="none" disabled>Select City</option>
+            {cities.map((val, idx) => {
+              return <option value={val} key={idx}>{val}</option>;
+            })}
+          </select>
+        </div>
+        {selectedCity === "none" ? null:<p>You Selected {selectedCity}, {selectedState}, {selectedCountry}</p>}
+      </div>
+    </>
   );
 }
 
