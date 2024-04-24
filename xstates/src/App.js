b9756f872991d6ea3bd5e37a937b533d08d83e16
Ayush Gupta <gupta.ayush0111@gmail.com>
2024-04-24 22:13:51 +0530

    Error handled for xstates

--- xstates/src/App.js
+++ xstates/src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
 function App() {
   const [countries, setCountries] = useState([]);
@@ -8,37 +8,49 @@ function App() {
   const [selectedState, setSelectedState] = useState("none");
   const [cities, setCities] = useState([]);
   const [selectedCity, setSelectedCity] = useState("none");
-  
+
   useEffect(() => {
     let isFlag = true;
     const fetchCity = async () => {
-      if(selectedState === "none") {
+      if (selectedState === "none" || selectedCountry === "none") {
         return;
       }
-      await fetch(`https://crio-location-selector.onrender.com/country=${selectedCountry}/state=${selectedState}/cities`)
-      .then((response) => response.json())
-      .then((data) => setCities(data))
-      .catch((error) => console.error("Error fetching cities: ", error));
-    }
+      await fetch(
+        `https://crio-location-selector.onrender.com/country=${selectedCountry}/state=${selectedState}/cities`
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          setCities(data);
+          setSelectedCity("none");
+        })
+        .catch((error) => console.error("Error fetching cities: ", error));
+    };
     if (isFlag) {
       fetchCity();
     }
     return () => {
       isFlag = false;
     };
-  }, [selectedState, selectedCountry]);  
+  }, [selectedState, selectedCountry]);
 
   useEffect(() => {
     let isFlag = true;
     const fetchState = async () => {
-      if(selectedCountry === "none") {
+      if (selectedCountry === "none") {
         return;
       }
-      await fetch(`https://crio-location-selector.onrender.com/country=${selectedCountry}/states`)
-      .then((response) => response.json())
-      .then((data) => setStates(data))
-      .catch((error) => console.error("Error fetching states: ", error));
-    }
+      await fetch(
+        `https://crio-location-selector.onrender.com/country=${selectedCountry}/states`
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          setStates(data);
+          setSelectedState("none");
+          setCities([]);
+          setSelectedCity("none");
+        })
+        .catch((error) => console.error("Error fetching states: ", error));
+    };
     if (isFlag) {
       fetchState();
     }
@@ -51,10 +63,10 @@ function App() {
     let isFlag = true;
     const fetchCountry = async () => {
       await fetch("https://crio-location-selector.onrender.com/countries")
-      .then((response) => response.json())
-      .then((data) => setCountries(data))
-      .catch((error) => console.error("Error fetching countries: ", error));
-    }
+        .then((response) => response.json())
+        .then((data) => setCountries(data))
+        .catch((error) => console.error("Error fetching countries: ", error));
+    };
     if (isFlag) {
       fetchCountry();
     }
@@ -67,26 +79,68 @@ function App() {
       <div className="main">
         <h1>Select Location</h1>
         <div className="dropdowns">
-          <select name="country" id="country" value={selectedCountry} onChange={(e) => setSelectedCountry(e.target.value)} className="dropdown">
-            <option value="none" disabled>Select Country</option>
+          <select
+            name="country"
+            id="country"
+            value={selectedCountry}
+            onChange={(e) => setSelectedCountry(e.target.value)}
+            className="dropdown"
+          >
+            <option value="none" disabled>
+              Select Country
+            </option>
             {countries.map((val, idx) => {
-              return <option value={val} key={idx}>{val}</option>;
+              return (
+                <option value={val} key={idx}>
+                  {val}
+                </option>
+              );
             })}
           </select>
-          <select name="state" id="state" disabled={selectedCountry === "none"?true:false} value={selectedState} onChange={(e) => setSelectedState(e.target.value)} className="dropdown">
-            <option value="none" disabled>Select State</option>
+          <select
+            name="state"
+            id="state"
+            disabled={selectedCountry === "none" ? true : false}
+            value={selectedState}
+            onChange={(e) => setSelectedState(e.target.value)}
+            className="dropdown"
+          >
+            <option value="none" disabled>
+              Select State
+            </option>
             {states.map((val, idx) => {
-              return <option value={val} key={idx}>{val}</option>;
+              return (
+                <option value={val} key={idx}>
+                  {val}
+                </option>
+              );
             })}
           </select>
-          <select name="city" id="city" disabled={selectedState === "none"?true:false} value={selectedCity} onChange={(e) => setSelectedCity(e.target.value)} className="dropdown">
-            <option value="none" disabled>Select City</option>
+          <select
+            name="city"
+            id="city"
+            disabled={selectedState === "none" ? true : false}
+            value={selectedCity}
+            onChange={(e) => setSelectedCity(e.target.value)}
+            className="dropdown"
+          >
+            <option value="none" disabled>
+              Select City
+            </option>
             {cities.map((val, idx) => {
-              return <option value={val} key={idx}>{val}</option>;
+              return (
+                <option value={val} key={idx}>
+                  {val}
+                </option>
+              );
             })}
           </select>
         </div>
-        {selectedCity === "none" ? null:<p>You Selected {selectedCity}, {selectedState}, {selectedCountry}</p>}
+        {selectedCity === "none" ? null : (
+          <p>
+            You selected {selectedCity}, {selectedState}, {selectedCountry}
+          </p>
+        )}
       </div>
     </>
   );
